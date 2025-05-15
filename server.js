4e2eb5875ecae5d0b71a6684bcc85d5971b3e45e
abdelrahman-anasr <abdulrahman.nasr@student.giu-uni.de>
2025-05-15 03:35:06 +0300

    Updated source IP acceptable

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ dotenv.config();
   const app = express();
 
   const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', '154.177.20.138'],
     credentials: true,
   };
   app.use(cors(corsOptions));
